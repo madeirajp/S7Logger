a4c640eae285c052b92a21230f2c7b89888de9ba
Max Angelma <Max Angelma>
2017-02-04 15:21:47 +0200

    Changed file names

---
+++ S7Logger.py
@@ -0,0 +1,240 @@
+# Only Bool-type of memory values supported!
+
+# for taglists.txt: show all columns, select all (CTRL+A) and paste to notepad
+# columns should be in following order: Name, Tag table, Data type, Address
+# tags export to xlsx is not supported
+
+# If there is need to debug, enable this
+#logging.basicConfig(level=logging.DEBUG)
+
+# IP-address for PLC
+ipaddress = "169.254.0.101"
+
+# Scan time
+scantime = 0.1
+
+# for offline developing
+offline = True
+
+# import some stuff from the Snap7 library
+import snap7.client as c
+from snap7.util import *
+from snap7.snap7types import *
+
+# time library for sleep function and for logging local time
+import time
+
+# sys library for nice console output
+import sys
+
+# create dict to store IO objects
+names = {}
+
+# because OOP is the way to go
+class IOObject:
+
+    # counter for how many IOs we are logging
+    IOCount = 0
+
+    # constructor
+    def __init__(self, name, dtype, ioType, byte, bit):
+        self.name = name
+        self.dtype = dtype
+        self.ioType = ioType
+        self.byte = byte
+        self.bit = bit
+        self.reading = 0
+        IOObject.IOCount += 1
+
+    # display the count
+    def displayCount(self):
+        print IOObject.IOCount
+
+    def returnName(self):
+        return self.name
+
+    def returnType(self):
+    	return self.dtype
+
+    def returnIoType(self):
+    	return self.ioType
+
+    def returnByte(self):
+    	return self.Byte
+
+    def returnBit(self):
+    	return self.Bit
+
+    # flag for edge detection
+    def setEdgeFlag(self):
+        self.edgeFlag = True
+
+    # flag for resetting
+    def resetEdgeFlag(self):
+        self.edgeFlag = False
+
+    def returnEdgeFlag(self):
+    	return self.edgeFlag
+
+    def readIO(self):
+
+    	# TODO: test this ONLINE
+    	if self.dtype != Bool:
+    		print "Only Boolean memory types are supported!"
+    		return
+    	# areas:
+    	# PA = Process outputs, PE = Process inputs, MK = Merkers
+    	if self.ioType == "Q":
+	    	result = plc.read_area(areas['PA'],0,int(self.byte),S7WLBit)
+        if self.ioType == "I":
+		    result = plc.read_area(areas['PE'],0,int(self.byte),S7WLBit)
+        if self.ioType == "M":
+		    result = plc.read_area(areas['MK'],0,int(self.byte),S7WLBit)
+
+	    # return the value
+        return get_bool(result,0,int(self.bit))
+
+    # for printing human-readable things about our objects
+    def __str__(self):
+        return "%s %s %s %s %s" % (self.name, self.dtype, self.ioType, self.byte, self.bit)
+
+
+# GLOBAL FUNCTIONS
+
+# this function will read the text file and save the IOs to dict
+def readTags():
+
+    # okay, lets open this file
+    taglist = open('taglist.txt', 'r')
+
+    # then loop through all of the lines
+    for line in taglist:
+
+        # and split them by line break
+        splitline = line.split('\n')
+
+        # how to not name variables.. 
+        name = splitline[0].strip().split('\t')
+
+        #print list(name[0])
+
+        # ignore files starting with hashtag
+        if not list(name[0])[0] == "#":
+
+        	# sorry about this mess, memory addresses were tough for me
+			address = name[3].split(".")
+			#print address
+			dtype = name[2]
+			addresslist = list(address[0])
+			ioType = addresslist[1]
+			byte = ''.join(addresslist[2:])
+			
+			# Memory types without byte,bit type of address are not supported
+			if len(address) > 1:
+				# prevent indexerror
+				bit = address[1]
+				# call the constructor
+				names[name[0]] = IOObject(name[0], dtype, ioType, byte, bit)
+			# let the user know if there is any unsupported type of tags
+			else: 
+				# instructions for user
+				print "\nNOTICE: " + name[2] + " is not supported!"			
+
+def logToFile():
+
+	# loop through objects
+	# TODO: not oPuts!
+    for oPut in names:
+
+    	# renaming 
+    	thisIO = names[oPut]
+
+    	# read the certain IO
+    	value = thisIO.readIO()
+
+        # time formatting
+        datestamp = time.strftime('%Y-%m-%d')
+        timestamp = time.strftime('%H:%M:%S')
+
+        # Logging only if this IO is True and edge flag is on
+        if value == True and thisIO.returnEdgeFlag():
+
+            # write to file the name and type
+            f.write(thisIO.returnName() + "," + thisIO.returnIoType())
+
+            # and of cource date- and timestamp, duh
+            f.write("," + datestamp + "," + timestamp + '\n')
+
+            # also print something to console
+            print('\n' + thisIO.returnName() + " event logged")
+            
+            # reset the edge flag
+            thisIO.resetEdgeFlag()
+
+        # From falling edge set the edge flag
+        if value == False:
+        	thisIO.setEdgeFlag()
+
+
+# MAIN LOOP
+if __name__=="__main__":
+
+    # Create client
+    plc = c.Client()
+    
+    # if we are offline we do not try to conncet
+    if offline == False:
+
+    	# arguments are for rack 0, slot 2
+    	plc.connect(ipaddress,0,2)
+
+    # read the tags
+    readTags()
+
+    # print all the IO objects to console just to make sure 
+    print "These " + str(IOObject.IOCount) + " memory addresses will be logged: "
+    for x in names:
+        print "  " + str(names[x])
+
+    # just a little fun to console.. sorry I was tired
+    print "Initializing",
+    for x in range(0, IOObject.IOCount):
+    	print ".",
+    	time.sleep(scantime)
+
+    # instructions for the user
+    print '\n' + "Use CTRL + C to end logging." + '\n'
+
+    try:
+	    # Scanning loop
+	    while True:
+	        
+	        # time formatting
+	        timestamp = time.strftime('%H:%M:%S')
+
+	        # Let the user know that scanning is active
+	        sys.stdout.write('\r Scanning PLC memory on ' + timestamp + ' with the scan time of ' + str(scantime) + ' seconds')  
+	        sys.stdout.flush()
+
+	        # Open the file where we log the data
+	        f = open('workfile.txt', 'a')
+
+	        # if we are online, lets log
+	        if offline == False:
+	        	logToFile()
+
+	        # Close the workfile so the logged data can be viewed while the script is running
+	        f.close()
+
+	        # Wait for five seconds
+	        time.sleep(scantime)
+
+	# to break out from loop in console (CTRL+C)
+    except KeyboardInterrupt:
+    	logFile = open('workfile.txt', 'r')
+    	print "\n \n" + "Thank you for using S7Logger! Here are the contents of the workfile: " + "\n"
+    	for line in logFile:
+    		print line
+        plc.disconnect()
+        logFile.close()
+        print "Workfile closed. Bye!"
---
+++ S7Visualizer.js
@@ -0,0 +1,261 @@
+// Hello, this is the javascript for reading the logs and providing them to Highcharts
+
+// where CSV file is stored
+var URL = "https://dl.dropboxusercontent.com/u/3685401/S7Logger/workfile.txt"
+var dayObjects = []; // array to store days
+
+// data object
+var DayObject = {
+    init: function (day, allArray) {
+        this.day = day;
+        this.allArray = [];
+        this.actionArray = [];
+        this.countArray = [];
+
+    },
+    describe: function () {
+        var description = this.day;
+        return description;
+    },
+    showArray: function () {
+        var items = this.allArray;
+        return items;
+    },
+    addAction: function (action) {
+        this.actionArray = action;
+    },
+    addCounts: function (counts) {
+        this.countArray = counts;
+    },
+    returnActions: function() {
+        return this.actionArray;
+    },
+    returnCounts: function() {
+        return this.countArray;
+    }
+};
+
+function getFileFromServer(url, doneCallback) {
+    var xhr;
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleStateChange;
+    xhr.open("GET", url, true);
+    xhr.send();
+
+    // I don't know what this does
+    function handleStateChange() {
+        if (xhr.readyState === 4) {
+            doneCallback(xhr.status == 200 ? xhr.responseText : null);
+        } } }
+
+getFileFromServer(URL, function(text) {
+    // if file is not found, display error message
+    if (text === null) { console.log("404 ") }
+    // if file is found, call function
+    else { textToObjects(text); }
+    } );
+
+// function for modifying the CSV file to objects
+function textToObjects(text) {
+
+    // split text into lines by \n (line break)
+    var line = text.split('\n');
+
+    // loop every line
+    for (var i = 0; i < line.length; i++)  {
+
+        // ignore empty lines if there is any
+        if (line[i].length != 1) {
+
+            //split every line by comma
+            var linesplit = line[i].split(",");
+
+            // find values
+            // example line: "vacuumOn,I,2017-01-23,16:07:01"
+            var output = linesplit[0];
+            var type = linesplit[1];
+            var day = linesplit[2];
+            var time = linesplit[3];
+            var thisDay = Object.create(DayObject);
+
+            // first day
+            if (dayObjects.length == 0) {
+                // just create day object
+                thisDay.init(day);
+                // and push it to array
+                dayObjects.push(thisDay);
+                // push to array
+                thisDay.allArray.push(output);
+
+                // after the first day, just push unique dates to array
+                // checkUnique will push if there are multiple entries on same day
+            } else if (checkUnique(day, output)) { // if index i.e. day not found
+                // create day object
+                thisDay.init(day);
+                dayObjects.push(thisDay);
+                thisDay.allArray.push(output);
+            }
+        }
+    }
+
+    // call the visualizing function
+    unitTest(); // check the array
+    dateArray = objectsToArrays();
+    actionCountArrays = joinActionsAndCounts();
+    visualize(dateArray);
+    //dataToArrays();
+    //printAllDates();
+
+
+}
+
+// Unique checking function to days
+function checkUnique(daytocheck, output) {
+    for (var i=0; i < dayObjects.length; i++) {
+        // if day is found in dayObjects, return false
+        //console.log(dayObjects[i].thisDay);
+        if (dayObjects[i].day == daytocheck) {
+            //console.log("Multiple entries found for " + daytocheck);
+            //console.log("Data pushed to array: " + output)
+            dayObjects[i].allArray.push(output);
+            return false;
+        // if not found, return true
+        } else {
+            //console.log("Unique day found" + daytocheck);
+    } }
+    return true;
+}
+
+// to keep track what we have, dont touch
+function printAllDates() {
+    console.log("Dates and their data: ");
+    dayObjects.forEach(function (x) {
+        console.log(x.describe(), x.showArray());
+    });
+}
+
+function objectsToArrays() {
+    dateArray = [];
+    dayObjects.forEach(function (x) {
+        dateArray.push(x.describe());
+        var actionsAndCounts = countActions(x.showArray(), x.describe());
+        x.addAction(actionsAndCounts[0]);
+        x.addCounts(actionsAndCounts[1]);
+    });
+    return dateArray;
+}
+
+/*
+function dataToArrays() {
+
+    //console.log(dayObjects.length);
+
+    dataArray = [];
+
+    // loop through dayObjects
+    for (var i=0; i < dayObjects.length; i++) {
+
+        var arrayOfThisDay = dayObjects[i].array;
+
+        console.log(dayObjects[i]);
+
+        //console.log(arrayOfThisDay.length);
+
+        // loop through actions of this day
+        for (var j=0; j < arrayOfThisDay.length; j++) {
+
+            //console.log(arrayOfThisDay[j]);
+
+
+
+            dataArray.push(arrayOfThisDay[j]);
+        }
+    }
+    //console.log(dataArray);
+} */
+
+// source: http://stackoverflow.com/questions/5667888/counting-the-occurrences-of-javascript-array-elements
+function countActions(arr, day) {
+    var a = [], b = [], prev;
+
+    arr.sort();
+    for ( var i = 0; i < arr.length; i++ ) {
+        if ( arr[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = arr[i];
+    }
+    return [a, b];
+}
+
+function unitTest() {
+    for (var i = 0; i < dayObjects.length; i++) {
+        if (dayObjects[i].allArray.length == 0) {
+            throw "Invalid array item with no data";
+        }
+    }
+}
+
+function joinActionsAndCounts() {
+
+    dayObjects.forEach(function (x) {
+
+        console.log(x.returnActions());
+
+        actionArray = [];
+        countArray = [];
+
+    });
+
+
+}
+
+// visualizing function
+function visualize(datearray) {
+
+    // using Highcharts line-basic example here
+    Highcharts.chart('container', {
+        title: {
+            text: 'Maxin kandi',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'https://github.com/mangelma/S7Logger',
+            x: -20
+        },
+        xAxis: {
+
+            // array of days here
+            categories: datearray
+        },
+        yAxis: {
+
+            // only integers
+            allowDecimals: false,
+
+            title: {
+                text: 'Highs'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+
+        // output arrays are here:
+        series: [{
+            name: 'Vacuum',
+            data: [1, 3, 2, 8, 4]
+        }]
+    });
+}
