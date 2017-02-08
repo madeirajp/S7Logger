45be3f6fcf07c9df5d03e08e87472848a03e6e21
Max Angelma <Max Angelma>
2017-02-08 13:41:07 +0200

    separate config file

---
+++ S7Config.js
@@ -0,0 +1,3 @@
+// For further development, I thought a separate config file would be nice
+// Right now it only hosts logUrl variable:
+var logUrl = "https://dl.dropboxusercontent.com/u/3685401/S7Logger/log.csv"
